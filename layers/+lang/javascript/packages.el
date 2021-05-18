61fc1c633b76d16768b32a61e4bcbb7c88cebbd8
Xun Zhu <zhuxun2@gmail.com>
2021-05-17 23:21:30 -0500

    [javascript] Activate the javascript layer even in typescript files

--- layers/+lang/javascript/packages.el
+++ layers/+lang/javascript/packages.el
@@ -112,7 +112,7 @@
 (defun javascript/init-js2-mode ()
   (use-package js2-mode
     :defer t
-    :mode (("\\.m?js\\'"  . js2-mode))
+    :mode (("\\.m?[jt]s\\'"  . js2-mode))
     :init
     (progn
       (add-hook 'js2-mode-local-vars-hook #'spacemacs//javascript-setup-backend)
